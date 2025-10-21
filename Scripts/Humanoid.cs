c482a5620836b93297a9d1fb652941e8816b9ab5
Natelytle <nathenbr@gmail.com>
2025-10-20 23:09:59 -0400

    Start work on Humanoid script, implement ground raycasts

---
+++ Scripts/Humanoid.cs
@@ -0,0 +1,125 @@
+using System;
+using Godot;
+
+namespace RobloxCharacterController.Scripts;
+
+public partial class Humanoid : RigidBody3D
+{
+	[ExportGroup("Movement")] 
+	[Export] public float WalkSpeed { get; private set; } = 16f;
+	[Export] public float JumpPower { get; private set; } = 53f;
+
+	private RayCast3D _ceilingRayCast = null!;
+	private RayCast3D _groundRayCast = null!;
+	private RayCast3D _climbRayCast = null!;
+
+	// Floor properties
+	public float? FloorDistance { get; private set; }
+	public Vector3? FloorNormal { get; private set; }
+	public Vector3? FloorHitLocation { get; private set; }
+	public GodotObject? FloorPart { get; private set; }
+	public PhysicsMaterial? FloorMaterial { get; private set; }
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_ceilingRayCast = new RayCast3D();
+		AddChild(_ceilingRayCast);
+
+		_groundRayCast = new RayCast3D();
+		AddChild(_groundRayCast);
+		
+		_climbRayCast = new RayCast3D();
+		AddChild(_climbRayCast);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		SetFloorProperties();
+	}
+
+	private void SetFloorProperties()
+	{
+		float[] xPositions = [0, 0.8f, -0.8f];
+		float[] zPositions = [0, -0.45f, 0.45f];
+		const float yPosition = -0.9f;
+		
+		// Get the raycast length depending on if we had a floor last frame.
+		float length = FloorDistance is not null ? 1.5f : 1.1f;
+		length += Math.Abs(LinearVelocity.Y) > 100 ? Math.Abs(LinearVelocity.Y) / 100.0f : 0;
+		length = length * 2 + 1;
+
+		_groundRayCast.TargetPosition = new Vector3(0, -length, 0);
+		
+		// Reset floor info
+		FloorDistance = null;
+		FloorNormal = null;
+		FloorHitLocation = null;
+		FloorMaterial = null;
+		FloorPart = null;
+
+		float sum = 0;
+		int count = 0;
+
+		// Check the center, then the sides.
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				// We skip the center raycast on the sides..
+				if (i > 0 && j == 0)
+					continue;
+
+				_groundRayCast.Position = new Vector3(xPositions[i], yPosition, zPositions[j]);
+				_groundRayCast.ForceRaycastUpdate();
+
+				if (_groundRayCast.IsColliding())
+				{
+					sum += _groundRayCast.GlobalPosition.DistanceTo(_groundRayCast.GetCollisionPoint());
+					count++;
+
+					FloorNormal ??= _groundRayCast.GetCollisionNormal();
+					FloorHitLocation ??= _groundRayCast.GetCollisionPoint();
+					FloorPart ??= _groundRayCast.GetCollider();
+
+					if (FloorMaterial is null && FloorPart is not null)
+					{
+						FloorMaterial = FloorPart switch
+						{
+							StaticBody3D s => s.PhysicsMaterialOverride,
+							RigidBody3D r => r.PhysicsMaterialOverride,
+							_ => FloorMaterial
+						};
+					}
+				}
+			}
+
+			if (count != 0)
+				break;
+		}
+		
+		const float zPositionSecondary = 0.85f;
+
+		// We have 2 more checks, just do em manually
+		for (int i = -1; i < 2; i += 2)
+		{
+			_groundRayCast.Position = new Vector3(0, yPosition, i * zPositionSecondary);
+			_groundRayCast.ForceRaycastUpdate();
+
+			if (_groundRayCast.IsColliding())
+			{
+				sum += _groundRayCast.GlobalPosition.DistanceTo(_groundRayCast.GetCollisionPoint());
+				count++;
+			}
+		}
+
+		if (count > 0)
+			FloorDistance = sum / count;
+	}
+}
